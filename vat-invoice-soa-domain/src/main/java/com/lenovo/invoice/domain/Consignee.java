b213b31853ca6225bc0123cd03cb5f00dc94ebe6
mayan3 <mayan3@lenovo.com>
2017-05-15 13:49:57 +0800

    添加修改收票地址接口

---
+++ vat-invoice-soa-domain/src/main/java/com/lenovo/invoice/domain/Consignee.java
@@ -0,0 +1,82 @@
+package com.lenovo.invoice.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by mayan3 on 2017/5/15.
+ */
+public class Consignee implements Serializable {
+    private String name;
+    private String provinceid;
+    private String city;
+    private String county;
+    private String address;
+    private String phone;
+    private String zip;
+    private String orderCode;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProvinceid() {
+        return provinceid;
+    }
+
+    public void setProvinceid(String provinceid) {
+        this.provinceid = provinceid;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getOrderCode() {
+        return orderCode;
+    }
+
+    public void setOrderCode(String orderCode) {
+        this.orderCode = orderCode;
+    }
+}
