030235a12f44c37d36c0c24dd66ddcd912a1f62e
xuweihua <xuwh3@lenovo.com>
2017-07-26 14:22:36 +0800

    印度摩托

--- vat-invoice-soa-domain/src/main/java/com/lenovo/invoice/domain/result/DeliverGoodsTypeEnum.java
+++ vat-invoice-soa-domain/src/main/java/com/lenovo/invoice/domain/result/DeliverGoodsTypeEnum.java
@@ -4,18 +4,18 @@ package com.lenovo.invoice.domain.result;
  * Created by xuweihua on 2017/7/26.
  */
 public enum DeliverGoodsTypeEnum {
-    AIRE_MODE(1,"GL Expedite – Air Mode"),//空运
-    SURFACE_MODE(2,"GL Standard – Surface Mode"),//陆运
-    SELF_PICK(3,"Self Pick up – Customer will come and pick up");//自取
+    AIRE_MODE("L1","GL Expedite – Air Mode"),//空运
+    SURFACE_MODE("L2","GL Standard – Surface Mode"),//陆运
+    SELF_PICK("S2","Self Pick up – Customer will come and pick up");//自取
 
-    private DeliverGoodsTypeEnum(int type, String descr) {
+    private DeliverGoodsTypeEnum(String type, String descr) {
         this.type = type;
         this.descr = descr;
     }
-    private final int type;
+    private final String type;
     private final String descr;
 
-    public int getType() {
+    public String getType() {
         return type;
     }
 
